c87981e6a0be1c61f0c2c0485899cfe17e9d359e
Moemu <WhitemuTeam@outlook.com>
2025-03-05 22:11:40 +0800

    Feat: Add Metadata

--- nonebot_plugin_muicebot/__init__.py
+++ nonebot_plugin_muicebot/__init__.py
@@ -4,17 +4,18 @@ require("nonebot_plugin_alconna")
 require("nonebot_plugin_localstore")
 require("nonebot_plugin_apscheduler")
 
-from nonebot.plugin import PluginMetadata  # noqa: E402
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters  # noqa: E402
 
 from .config import Config  # noqa: E402
 from .onebot import *  # noqa: E402, F403, F401
 
 __plugin_meta__ = PluginMetadata(
-    name="MuiceAI Plugin",
-    description="沐雪，一个不会自动找你聊天的AI女孩子",
-    usage="瞎几把用",
+    name="MuiceBot",
+    description="Muice-Chatbot 的 Nonebot2 实现，支持市面上大多数的模型",
+    usage="@at / {config.MUICE_NICKNAMES} <message>: 与大语言模型交互；关于指令类可输入 .help 查询",
     type="application",
     config=Config,
     homepage="https://bot.snowy.moe/",
     extra={},
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
 )
