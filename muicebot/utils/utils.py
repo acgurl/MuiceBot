e04a0cfb7545ffb4a2c96eea68d689113806e259
Moemu <WhitemuTeam@outlook.com>
2025-03-30 17:02:54 +0800

    Feat: 添加版本号和数据目录显示

--- muicebot/utils/utils.py
+++ muicebot/utils/utils.py
@@ -3,6 +3,7 @@ import os
 import ssl
 import sys
 import time
+from importlib.metadata import PackageNotFoundError, version
 from typing import Optional
 
 import httpx
@@ -131,3 +132,41 @@ def init_logger():
         rotation="1 day",
         retention="7 days",
     )
+
+
+def get_version() -> str:
+    """
+    获取当前版本号
+
+    优先尝试从已安装包中获取版本号, 否则从 `pyproject.toml` 读取
+    """
+    package_name = "muicebot"
+
+    try:
+        return version(package_name)
+    except PackageNotFoundError:
+        pass
+
+    toml_path = os.path.join(os.path.dirname(__file__), "../pyproject.toml")
+
+    if not os.path.isfile(toml_path):
+        return "Unknown"
+
+    try:
+        if sys.version_info >= (3, 11):
+            import tomllib
+
+            with open(toml_path, "rb") as f:
+                pyproject_data = tomllib.load(f)
+
+        else:
+            import toml
+
+            with open(toml_path, "r", encoding="utf-8") as f:
+                pyproject_data = toml.load(f)
+
+        # 返回版本号
+        return pyproject_data["tool"]["pdm"]["version"]
+
+    except (FileNotFoundError, KeyError, ModuleNotFoundError):
+        return "Unknown"
