eda9ce8b2984727cf9f90575e4f60a9a7dc527db
Moemu <WhitemuTeam@outlook.com>
2025-06-07 19:30:04 +0800

    我对我的代码质量很有信心

--- tests/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class BotInfo:
-    self_id: int = 123456
-    superuser_id: int = 100000
-    normaluser_id: int = 100001
-    message_id: int = 200001
