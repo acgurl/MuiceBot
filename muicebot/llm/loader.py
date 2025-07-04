645a4a191fdd76b1d65b88a5125729d7c3a6a599
Moemu <WhitemuTeam@outlook.com>
2025-07-04 19:53:48 +0800

    Feat: 优化模型类加载方法，使其可插件化

--- muicebot/llm/loader.py
+++ muicebot/llm/loader.py
@@ -1,4 +1,7 @@
 import importlib
+from importlib.util import find_spec
+
+from nonebot import logger
 
 from ._base import BaseLLM
 from ._config import ModelConfig
@@ -9,11 +12,14 @@ def load_model(config: ModelConfig) -> BaseLLM:
     """
     获得一个 LLM 实例
     """
-    module_name = config.loader.lower()
-    module_path = f"muicebot.llm.providers.{module_name}"
+    provider = config.loader.lower()
 
-    # 延迟导入模型模块（只导一次）
-    importlib.import_module(module_path)
+    # 如果是内置模型提供者，需要先导入
+    # 否则视为已导入的插件
+    builtin_provider = f"muicebot.llm.providers.{provider}"
+    if find_spec(builtin_provider) is not None:
+        logger.debug(f"加载内嵌模型模块: {provider}...")
+        importlib.import_module(builtin_provider)
 
     # 注册之后，直接取类使用
     LLMClass = get_llm_class(config.loader)
